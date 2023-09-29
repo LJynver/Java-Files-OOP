c0cd058bc402c33fee014c3d6309932981feeb67
LJynver <Mrlyanethan@gmail.com>
2023-09-29 14:18:28 +0800

    Showed Task#1

---
+++ ExhaustCPU/README.md
@@ -0,0 +1,11 @@
+# First execute
+The CPU usage rose from 0% to 60%
+Last cancellation and it dipped to 7% usage
+
+# Second Execute
+The CPU usage increased to 70%
+When cancelled after third, it decreased to 40%
+
+# Third Execute
+The CPU usage goes to 90%. I am even writing this while the CPU was high af.
+I started to cancel the process from here, it decreased to 50-60%
--- ExhaustCPU/src/exhaustcpu/ExhaustCPU.java
+++ ExhaustCPU/src/exhaustcpu/ExhaustCPU.java
@@ -1,9 +1,24 @@
-
 package exhaustcpu;
+
+class HelloRunner implements Runnable {
+    int i;
+    public void run() {
+        i = 0;
+        while (true) {
+            System.out.println("Hello " + i);
+            if (i == 50) {
+                break;
+            }
+        }
+    }
+}
 
 public class ExhaustCPU {
 
     public static void main(String[] args) {
+        HelloRunner testThread = new HelloRunner();
+        Thread a = new Thread(testThread);
+        a.start();
         
     }
     
